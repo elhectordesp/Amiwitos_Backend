d3a9582c05d17c2f75b286a77fec521f68bc4e0b
elhectordesp <hectordura74@gmail.com>
2023-08-09 16:56:35 +0200

    feat: register and login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,28 +2,34 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const verifyToken = require('./authentication');
-const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+const pool = require('../config/pool-config');
 
 const authCtrl = {};
 
 authCtrl.login = async (req, res) => {
     console.log(req.body);
-    await pool.query(
-        'SELECT * FROM usuarios WHERE email = ' + req.body.email,
-        (error, result) => {
-            if (error) {
-                console.log(error.message);
-            }
-            if (!result) {
-                res.status(404).send('User not found');
-            } else if (req.body.password != result.password) {
-                return res.status(401).send({ auth: false, token: null });
-            } else {
-                const token = jwt.sign({ user: result}, config.secret, { expiresIn: 60*60*24*365 });
-                res.send({ auth: true, token });
-            }
+
+    try {
+        const query = `SELECT * FROM users WHERE email = $1`;
+        const result = await pool.query(query, [req.body.email]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).send('User not found');
         }
-    )
+
+        const user = result.rows[0];
+
+        if (req.body.password != user.password) {
+            return res.status(401).send({ auth: false, token: null });
+        }
+
+        const token = jwt.sign({ user }, config.secret, { expiresIn: '1y' });
+        res.send({ auth: true, token });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 authCtrl.logout = async (req, res) => {
